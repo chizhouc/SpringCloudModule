df4e0b716eb2f298cb89fefae9f2382189847d71
chizhouc <13545862545@163.com>
2020-06-04 16:30:00 +0800

    提交

---
+++ springcloudclientcallerdemo/src/test/java/com/example/springcloudclientcallerdemo/SpringcloudclientcallerdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudclientcallerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudclientcallerdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ springcloudfirstdemo/src/test/java/com/example/springcloudfirstdemo/SpringcloudfirstdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudfirstdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudfirstdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
